f1ee9e0219cde2dc18c5d32970cdfcde3eade571
Joseph <joseph@Joseph-Naccis-MacBook-Pro.local>
2023-03-12 11:44:17 +0100

    first commit

---
+++ app/src/main/java/com/example/aponwola/activities/LognRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aponwola.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aponwola.R
+
+class LognRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
